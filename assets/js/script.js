382e3e8e27fb73362d4fdbe4d4557969bf119b04
Jorge Aguilar <jorgeluis_942009@hotmail.com>
2021-02-23 19:10:56 -0600

    Responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,32 @@ $(function () {
     var idSala = $('.menuLateral .tab.listaSalas .sala.activa').data('id');
     pushMensaje(idSala, url, tipo);
   }
+  const navegador = () => {
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+   {
+       alert('Opera');
+   }
+   else if(navigator.userAgent.indexOf("Chrome") != -1 )
+   {
+       alert('Chrome');
+   }
+   else if(navigator.userAgent.indexOf("Safari") != -1)
+   {
+       alert('Safari');
+   }
+   else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+   {
+        alert('Firefox');
+   }
+   else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+   {
+     alert('IE'); 
+   }  
+   else 
+   {
+      alert('unknown');
+   }
+   }
 
 
   //---------- Acciones del publicador ----------
